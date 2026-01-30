e21f7a55f958d65a90a78db4fe6532c84a68a5c7
jm1021 <jmort1021@gmail.com>
2026-01-30 15:18:49 -0800

    changed gitignore to build file

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 rm -rf build
 mkdir build
 
-cd build
-
 sudo apt update
 sudo apt install libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxxf86vm-dev libgl1-mesa-dev
+
+cd build
 
 cmake ..
