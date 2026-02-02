38333f17ab4c85b8b90840c8d62392db0151f38b
Frogpants <spencerjl0309@gmail.com>
2026-02-02 11:16:02 -0800

    updated cmake and tri frag

--- src/renderer/shaders/fragTri.glsl
+++ src/renderer/shaders/fragTri.glsl
@@ -5,6 +5,6 @@ out vec4 FragColor;
 uniform sampler2D modelTexture;
 
 void main() {
-    vec3 color = vec3(abs(sin(vUV.x * 2.0) * cos(vUV.y)));
+    vec3 color = vec3(vUV.x,vUV.y,abs(sin(vUV.x + vUV.y)));
     FragColor = vec4(color, 1.0);
 }
